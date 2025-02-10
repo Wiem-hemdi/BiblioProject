74823e56ecd38fa99774b8bb5bd3b97955185c69
Saïd SASSI <sassi_said@live.fr>
2025-02-10 20:54:25 +0100

    Ajoutez des fichiers projet.

---
+++ GestionEtudiant/GestionEtudiantsForm.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+namespace GestionEtudiant
+{
+    public partial class GestionEtudiantsForm : Form
+    {
+        public GestionEtudiantsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAjouter_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException("buttonAjouter_Click");
+        }
+
+        private void buttonModifier_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException("buttonModifier_Click");
+        }
+
+        private void buttonSupprimer_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException("buttonSupprimer_Click");
+        }
+
+        private void buttonRechercher_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException("buttonRechercher_Click");
+        }
+
+        private void buttonTrierParNom_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException("buttonTrierParNom_Click");
+        }
+
+    }
+}
